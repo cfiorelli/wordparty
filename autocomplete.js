938c3ff6faa3c9a84974dbea9aaecddcf3e08e65
cfiorelli <christopherfiorelli@gmail.com>
2023-09-13 00:59:08 -0700

    words 3

--- autocomplete.js
+++ autocomplete.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
-    $("#search").autocomplete({
-        source: function(request, response) {
+    let currentTerm = '';
+    $("#search").on('input', function() {
+        const query = $(this).val();
+        if(query.length > 1 && currentTerm !== query) {
+            currentTerm = query;
             $.ajax({
                 url: "https://api.datamuse.com/sug",
                 dataType: "jsonp",
                 data: {
-                    s: request.term
+                    s: query,
+                    max: 1
                 },
                 success: function(data) {
-                    response($.map(data, function(item) {
-                        return {
-                            label: item.word,
-                            value: item.word
-                        };
-                    }));
+                    if(data.length > 0) {
+                        const suggestedWord = data[0].word;
+                        if(suggestedWord.startsWith(query)) {
+                            $('#shadowText').text(suggestedWord);
+                        }
+                    }
                 }
             });
-        },
-        minLength: 2
+        } else {
+            $('#shadowText').text(''); // Clear shadow text
+        }
+    }).on('keydown', function(e) {
+        if (e.keyCode === 9) { // If the key is Tab
+            e.preventDefault();
+            $(this).val($('#shadowText').text());
+            $('#shadowText').text('');
+        }
     });
 });
