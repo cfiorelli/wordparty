d53d2c8cca4a733d70e79bfb49e426cb0fc949e3
cfiorelli <christopherfiorelli@gmail.com>
2023-09-13 04:57:56 -0700

    removing shadowtext

--- autocomplete.js
+++ autocomplete.js
@@ -1,33 +1,26 @@
 $(document).ready(function() {
-    let currentTerm = '';
-    $("#search").on('input', function() {
-        const query = $(this).val();
-        if(query.length > 1 && currentTerm !== query) {
-            currentTerm = query;
+    $("#search").on('keyup', function() {
+        let query = $(this).val();
+        if (query.length >= 2) {  // Only fetch suggestions when 2 or more characters are typed
             $.ajax({
                 url: "https://api.datamuse.com/sug",
                 dataType: "jsonp",
                 data: {
-                    s: query,
-                    max: 1
+                    s: query
                 },
                 success: function(data) {
-                    if(data.length > 0) {
-                        const suggestedWord = data[0].word;
-                        if(suggestedWord.startsWith(query)) {
-                            $('#shadowText').text(suggestedWord);
-                        }
+                    if (data.length > 0) {
+                        // Display the word of the first suggestion below the textbox
+                        $('#suggestion').text(data[0].word);
+                    } else {
+                        // Clear the suggestion if there's no match
+                        $('#suggestion').text('');
                     }
                 }
             });
         } else {
-            $('#shadowText').text(''); // Clear shadow text
-        }
-    }).on('keydown', function(e) {
-        if (e.keyCode === 9) { // If the key is Tab
-            e.preventDefault();
-            $(this).val($('#shadowText').text());
-            $('#shadowText').text('');
+            // Clear the suggestion if less than 2 characters
+            $('#suggestion').text('');
         }
     });
 });
